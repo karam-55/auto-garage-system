d4e948b34573a44aab1b6bbdff7790b1c3217547
Auto Garage System <noreply@local>
2026-05-20 11:24:22 +0300

    إصلاح report providers لإرسال from و to parameters في URL

--- admin_frontend/lib/core/providers/report_providers.dart
+++ admin_frontend/lib/core/providers/report_providers.dart
@@ -30,18 +30,24 @@ final generalLedgerProvider = FutureProvider.autoDispose.family<Map<String, dyna
 // Advanced Reports Providers
 final cashFlowProvider = FutureProvider.autoDispose.family<Map<String, dynamic>, Map<String, DateTime>>((ref, params) async {
   final api = ref.read(apiServiceProvider);
-  final response = await api.get('/api/reports/cash-flow');
+  final fromDate = params['from'] as DateTime;
+  final toDate = params['to'] as DateTime;
+  final response = await api.get('/api/reports/cash-flow?from=${fromDate.toIso8601String().split('T')[0]}&to=${toDate.toIso8601String().split('T')[0]}');
   return response;
 });
 
 final breakEvenProvider = FutureProvider.autoDispose.family<Map<String, dynamic>, Map<String, DateTime>>((ref, params) async {
   final api = ref.read(apiServiceProvider);
-  final response = await api.get('/api/reports/break-even');
+  final fromDate = params['from'] as DateTime;
+  final toDate = params['to'] as DateTime;
+  final response = await api.get('/api/reports/break-even?from=${fromDate.toIso8601String().split('T')[0]}&to=${toDate.toIso8601String().split('T')[0]}');
   return response;
 });
 
 final tradingAccountProvider = FutureProvider.autoDispose.family<Map<String, dynamic>, Map<String, DateTime>>((ref, params) async {
   final api = ref.read(apiServiceProvider);
-  final response = await api.get('/api/reports/trading');
+  final fromDate = params['from'] as DateTime;
+  final toDate = params['to'] as DateTime;
+  final response = await api.get('/api/reports/trading?from=${fromDate.toIso8601String().split('T')[0]}&to=${toDate.toIso8601String().split('T')[0]}');
   return response;
 });
